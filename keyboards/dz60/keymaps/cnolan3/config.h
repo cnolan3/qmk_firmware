969738ede992f8e5b76c9d07d9b210d8816e0b0c
Connor Nolan <connor@cnolandev.com>
2024-09-09 09:40:51 -0700

    use space bar chording to activate hrm instead of tap-time

--- keyboards/dz60/keymaps/cnolan3/config.h
+++ keyboards/dz60/keymaps/cnolan3/config.h
@@ -1,2 +1,5 @@
 #define TAPPING_TERM 150
 #define PERMISSIVE_HOLD
+
+#define FORCE_NKRO
+#define COMBO_COUNT 8
--- keyboards/dz60/keymaps/cnolan3/keymap.c
+++ keyboards/dz60/keymaps/cnolan3/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 		MO(1),            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT,          LGUI_T( KC_Z ),    LALT_T( KC_X ),    LSFT_T( KC_C ),    LCTL_T( KC_V ),    KC_B,    KC_N,    RCTL_T( KC_M ),    RSFT_T( KC_COMM ), RALT_T( KC_DOT ),  RGUI_T( KC_SLSH ),          KC_RSFT,
+		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
 		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI,          KC_MENU, KC_RCTL),
 
 	LAYOUT_60_ansi(
@@ -15,4 +15,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_LEFT,   KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_HOME, KC_END,  KC_TRNS,   KC_TRNS,          KC_TRNS,
 		UG_TOGG, KC_TRNS,          KC_TRNS,                       KC_TRNS,                              KC_TRNS,  KC_TRNS,          KC_TRNS, KC_TRNS)
+};
+
+enum combos {
+    SPCA_LGUI,
+    SPCS_LALT,
+    SPCD_LSFT,
+    SPCF_LCTL,
+    SPCJ_RCTL,
+    SPCK_RSFT,
+    SPCL_RALT,
+    SPCSCLN_RGUI
+};
+
+const uint16_t PROGMEM spca_combo[] = {KC_SPC, KC_A, COMBO_END};
+const uint16_t PROGMEM spcs_combo[] = {KC_SPC, KC_S, COMBO_END};
+const uint16_t PROGMEM spcd_combo[] = {KC_SPC, KC_D, COMBO_END};
+const uint16_t PROGMEM spcf_combo[] = {KC_SPC, KC_F, COMBO_END};
+const uint16_t PROGMEM spcj_combo[] = {KC_SPC, KC_J, COMBO_END};
+const uint16_t PROGMEM spck_combo[] = {KC_SPC, KC_K, COMBO_END};
+const uint16_t PROGMEM spcl_combo[] = {KC_SPC, KC_L, COMBO_END};
+const uint16_t PROGMEM spcscln_combo[] = {KC_SPC, KC_SCLN, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+    [SPCA_LGUI] = COMBO(spca_combo, KC_LGUI),
+    [SPCS_LALT] = COMBO(spcs_combo, KC_LALT),
+    [SPCD_LSFT] = COMBO(spcd_combo, KC_LSFT),
+    [SPCF_LCTL] = COMBO(spcf_combo, KC_LCTL),
+    [SPCJ_RCTL] = COMBO(spcj_combo, KC_RCTL),
+    [SPCK_RSFT] = COMBO(spck_combo, KC_RSFT),
+    [SPCL_RALT] = COMBO(spcl_combo, KC_RALT),
+    [SPCSCLN_RGUI] = COMBO(spcscln_combo, KC_RGUI)
 };
